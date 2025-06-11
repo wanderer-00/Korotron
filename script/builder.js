f9b27ad1325f6b56596d7ad0fe4e81b1d021e48e
wanderer <56039464+wanderer-00@users.noreply.github.com>
2025-06-11 17:40:46 +0300

    Первичная вёрстка карточек

--- script/builder.js
+++ script/builder.js
@@ -1,4 +1,15 @@
 // кластер карточек
-let cluster = document.GetElementById("cluster");
+let cluster = document.getElementById("cluster");
 
-cluster.innerHTML = "бля";
+// внесение карточек в 'cluster'
+for(i=0;i<dataProgect111.length;i++){
+    // структура карточки товара
+    cluster.innerHTML += `
+        <div class="card">
+            <div class="icon" style="background-image: url(img/card_icon/apple.png)"></div>
+            <div class="name">${dataProgect111[i].NAME}</div>
+            <div class="color">${dataProgect111[i].COLOR}</div>
+            <div class="article">${dataProgect111[i].ARTICLE}</div>
+        </card>
+    `;
+};
