7c778229f9a521595b0c384419ddb77cd9ef7e3a
wanderer <56039464+wanderer-00@users.noreply.github.com>
2025-06-16 23:46:14 +0300

    -

--- page/test2/script/link.js
+++ page/test2/script/link.js
@@ -3,5 +3,8 @@ let menu = document.getElementById("menu");
 
 // построение списка категорий
 for (i = 0; i < links.length; i++) {
-    menu.innerHTML += `<a href="#">${links[i]}</a>`
+    menu.innerHTML += `
+        <svg class="m-ctlg-icon">
+            <use xlink:href="/img/icons-catalog.svg?v=004#${links[i].ID}"></use>
+        </svg>`
 };
