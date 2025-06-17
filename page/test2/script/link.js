23a06b2eaef92dcdd828c89de07a1d9e52d1f69a
wanderer <56039464+wanderer-00@users.noreply.github.com>
2025-06-17 16:44:34 +0300

    -

--- page/test2/script/link.js
+++ page/test2/script/link.js
@@ -4,7 +4,10 @@ let menu = document.getElementById("menu");
 // построение списка категорий
 for (i = 0; i < links.length; i++) {
     menu.innerHTML += `
-        <svg class="m-ctlg-icon">
-            <use xlink:href="/img/icons-catalog.svg?v=004#${links[i].ID}"></use>
-        </svg>`
+        <div title="${links[i].NAME}">
+            <input type="radio">
+            <svg class="m-ctlg-icon">
+                <use xlink:href="/img/icons-catalog.svg?v=004#${links[i].ID}"></use>
+            </svg>
+        </div>`
 };
