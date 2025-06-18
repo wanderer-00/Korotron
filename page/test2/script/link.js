e4e1259cbaa1877aeb056b3f516be4db22825ee3
wanderer <56039464+wanderer-00@users.noreply.github.com>
2025-06-18 17:29:48 +0300

    Отладка меню категорий

--- page/test2/script/link.js
+++ page/test2/script/link.js
@@ -5,9 +5,11 @@ let menu = document.getElementById("menu");
 for (i = 0; i < links.length; i++) {
     menu.innerHTML += `
         <div title="${links[i].NAME}">
-            <input type="radio">
-            <svg class="m-ctlg-icon">
-                <use xlink:href="/img/icons-catalog.svg?v=004#${links[i].ID}"></use>
-            </svg>
+                <input type="radio" id="A${i+1}" name="A">
+                <label for="A${i+1}">
+                    <svg class="m-ctlg-icon">
+                        <use xlink:href="/img/icons-catalog.svg?v=004#${links[i].ID}"></use>
+                    </svg>
+                </label>
         </div>`
 };
