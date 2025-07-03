0b0fb1d7c7f3889b2331fc30d7fd6625d6f207c3
wanderer <56039464+wanderer-00@users.noreply.github.com>
2025-07-03 18:19:49 +0300

    -

--- page/printer/accounting/data.js
+++ page/printer/accounting/data.js
@@ -5,21 +5,14 @@ var tableData = [
     ["XXXXXX", "Название организации", "Название организации", 1751500800000],
 
     // 2-ая таблица
+    // каждая строчка в таблице является услугой или товаром
     // синтаксис
-    // каждая строчка является услугой или товаром
-    // название товара или услуги | артикул | тираж | стоимость в формате числа с плавующей точкой
+    // название товара или услуги
+    // артикул | тираж | стоимость в формате числа с плавующей точкой
     [
-        [
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, minima, fugiat. Odio et atque fugit reiciendis odit placeat accusamus voluptates voluptas ullam, tempore aut eligendi deleniti itaque nobis culpa quis.",
-            "XXXXXXXX",
-            100,
-            100000.99
-        ],
-        [
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, minima, fugiat. Odio et atque fugit reiciendis odit placeat accusamus voluptates voluptas ullam, tempore aut eligendi deleniti itaque nobis culpa quis.",
-            "XXXXXXXX",
-            100,
-            100000.99
-        ],
+        ["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, minima, fugiat. Odio et atque fugit reiciendis odit placeat accusamus voluptates voluptas ullam, tempore aut eligendi deleniti itaque nobis culpa quis.",
+        "XXXXXXXX",100,100000.99],
+        ["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, minima, fugiat. Odio et atque fugit reiciendis odit placeat accusamus voluptates voluptas ullam, tempore aut eligendi deleniti itaque nobis culpa quis.",
+        "XXXXXXXX",100,100000.99],
     ]
-];
+]
